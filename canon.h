20aa1afcc7c614b8f281314baf4e0d80fe2302ac
Zecheng Li <lzecheng98@gmail.com>
2024-12-19 15:31:51 +0000

    use the writer's version

--- canon.h
+++ canon.h
@@ -1,9 +1,12 @@
+/*
+ * canon.h - Definitions for canon.c which converts the IR trees into
+ *           basic blocks and traces.
+ *
+ */
 #ifndef CANON_H
 #define CANON_H
 
-#include "temp.h"
 #include "translate.h"
-#include "util.h"
 
 typedef struct C_stmListList_ *C_stmListList;
 struct C_block {
@@ -15,58 +18,36 @@ struct C_stmListList_ {
   C_stmListList tail;
 };
 
-
-T_stmList C_linearize(T_stm stm);
 /* From an arbitrary Tree statement, produce a list of cleaned trees
-satisfying the following properties:
-1.  No SEQ's or ESEQ's
-2.  The parent of every CALL is an EXP(..) or a MOVE(TEMP t,..)
+   satisfying the following properties:
+      1.  No SEQ's or ESEQ's
+      2.  The parent of every CALL is an EXP(..) or a MOVE(TEMP t,..)
 */
+T_stmList C_linearize(T_stm stm);
 
-struct C_block C_basicBlocks(T_stmList stmList);
 /* basicBlocks : Tree.stm list -> (Tree.stm list list * Tree.label)
- From a list of cleaned trees, produce a list of
-basic blocks satisfying the following properties:
-1. and 2. as above;
-3.  Every block begins with a LABEL;
+       From a list of cleaned trees, produce a list of
+ basic blocks satisfying the following properties:
+      1. and 2. as above;
+      3.  Every block begins with a LABEL;
       4.  A LABEL appears only at the beginning of a block;
       5.  Any JUMP or CJUMP is the last stm in a block;
       6.  Every block ends with a JUMP or CJUMP;
    Also produce the "label" to which control will be passed
    upon exit.
 */
+struct C_block C_basicBlocks(T_stmList stmList);
 
-T_stmList C_traceSchedule(struct C_block b);
 /* traceSchedule : Tree.stm list list * Tree.label -> Tree.stm list
    From a list of basic blocks satisfying properties 1-6,
    along with an "exit" label,
-produce a list of stms such that:
-1. and 2. as above;
+   produce a list of stms such that:
+     1. and 2. as above;
      7. Every CJUMP(_,t,f) is immediately followed by LABEL f.
    The blocks are reordered to satisfy property 7; also
-in this reordering as many JUMP(T.NAME(lab)) statements
+   in this reordering as many JUMP(T.NAME(lab)) statements
    as possible are eliminated by falling through into T.LABEL(lab).
 */
+T_stmList C_traceSchedule(struct C_block b);
 
-static bool isNop(T_stm x);
-static bool commute(T_stm x, T_stm y);
-
-typedef struct expRefList_ *expRefList;
-struct expRefList_ {
-  T_exp *head;
-  expRefList tail;
-};
-struct stmExp {
-  T_stm s;
-  T_exp e;
-};
-
-static T_stm reorder(expRefList rlist);
-static T_stm do_stm(T_stm stm);
-static struct stmExp do_exp(T_exp exp);
-
-static expRefList ExpRefList(T_exp *, expRefList);
-static struct stmExp StmExp(T_stm, T_exp);
-static T_stm seq(T_stm, T_stm);
-
-#endif // CANON_H
+#endif
