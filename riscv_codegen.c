5c187aa3ff1dc90c2c81550b37768130744cd532
WilliamQiufeng <williamqiufeng@outlook.com>
2024-12-31 15:35:14 +0000

    append newline

--- riscv_codegen.c
+++ riscv_codegen.c
@@ -29,7 +29,7 @@ static void munchStm(T_stm s) {
             dst->u.MEM->u.BINOP.right->kind == T_CONST) {
           /* MOVE(MEM(BINOP(PLUS,e1,CONST(i))),e2) */
           T_exp e1 = dst->u.MEM->u.BINOP.left, e2 = src;
-          S("sw `s1, %d(`s0)", dst->u.MEM->u.BINOP.right->u.CONST);
+          S("sw `s1, %d(`s0)\n", dst->u.MEM->u.BINOP.right->u.CONST);
           emit(AS_Oper(strdup(buf), NULL,
                        L(munchExp(e1), L(munchExp(e2), NULL)), NULL));
         } else if (dst->u.MEM->kind == T_BINOP &&
@@ -37,7 +37,7 @@ static void munchStm(T_stm s) {
                    dst->u.MEM->u.BINOP.left->kind == T_CONST) {
           /* MOVE(MEM(BINOP(PLUS, CONST(i),e1)),e2) */
           T_exp e1 = dst->u.MEM->u.BINOP.right, e2 = src;
-          S("sw `s1, %d(`s0)", dst->u.MEM->u.BINOP.left->u.CONST);
+          S("sw `s1, %d(`s0)\n", dst->u.MEM->u.BINOP.left->u.CONST);
           emit(AS_Oper(strdup(buf), NULL,
                        L(munchExp(e1), L(munchExp(e2), NULL)), NULL));
         } else if (src->kind == T_MEM) {
@@ -45,21 +45,21 @@ static void munchStm(T_stm s) {
           // FIXME maybe we should ignore this case?
           T_exp e1 = dst->u.MEM, e2 = src->u.MEM;
           Temp_temp t = Temp_newtemp();
-          emit(AS_Oper("lw `d0, 0(`s0)", L(t, NULL), L(munchExp(e2), NULL),
+          emit(AS_Oper("lw `d0, 0(`s0)\n", L(t, NULL), L(munchExp(e2), NULL),
                        NULL));
-          emit(AS_Oper("sw `d0, 0(`s1)", L(t, NULL), L(munchExp(e1), NULL),
+          emit(AS_Oper("sw `d0, 0(`s1)\n", L(t, NULL), L(munchExp(e1), NULL),
                        NULL));
         } else {
           /* MOVE(MEM(e1),e2) */
           T_exp e1 = dst->u.MEM, e2 = src;
-          emit(AS_Oper("sw `s1 0(`s0)", NULL,
+          emit(AS_Oper("sw `s1 0(`s0)\n", NULL,
                        L(munchExp(e1), L(munchExp(e2), NULL)), NULL));
         }
       } else if (dst->kind == T_TEMP) {
         /* MOVE(TEMP(i),e2) */
         T_exp e2 = src;
         Temp_temp i = dst->u.TEMP;
-        emit(AS_Move("addi `d0, `s0, 0", L(i, NULL), L(munchExp(e2), NULL)));
+        emit(AS_Move("addi `d0, `s0, 0\n", L(i, NULL), L(munchExp(e2), NULL)));
       } else {
         assert(0);
       }
@@ -68,10 +68,10 @@ static void munchStm(T_stm s) {
     case T_JUMP: {
       if (s->u.JUMP.exp->kind == T_NAME) {
         /* JUMP(LABEL(t)) */
-        emit(AS_Oper("j `j0", NULL, NULL, AS_Targets(s->u.JUMP.jumps)));
+        emit(AS_Oper("j `j0\n", NULL, NULL, AS_Targets(s->u.JUMP.jumps)));
       } else {
         /* JUMP(e) */
-        emit(AS_Oper("j `s0", NULL, L(munchExp(s->u.JUMP.exp), NULL),
+        emit(AS_Oper("j `s0\n", NULL, L(munchExp(s->u.JUMP.exp), NULL),
                      AS_Targets(s->u.JUMP.jumps)));
       }
       break;
@@ -91,8 +91,8 @@ static void munchStm(T_stm s) {
       Temp_temp r1 = Temp_newtemp();
       Temp_temp r2 = Temp_newtemp();
       // FIXME: Do we need this safe copy?
-      emit(AS_Move("sd `s0,`d0", L(r1, NULL), L(e1, NULL)));
-      emit(AS_Move("sd `s0,`d0", L(r2, NULL), L(e2, NULL)));
+      emit(AS_Move("sd `s0,`d0\n", L(r1, NULL), L(e1, NULL)));
+      emit(AS_Move("sd `s0,`d0\n", L(r2, NULL), L(e2, NULL)));
 
       char *op_code;
       switch (s->u.CJUMP.op) {
@@ -107,7 +107,7 @@ static void munchStm(T_stm s) {
       S("%s `j0", op_code);
       emit(AS_Oper(strdup(buf), NULL, NULL,
                    AS_Targets(Temp_LabelList(s->u.CJUMP.true, NULL))));
-      emit(AS_Oper("j `j0", NULL, NULL,
+      emit(AS_Oper("j `j0\n", NULL, NULL,
                    AS_Targets(Temp_LabelList(s->u.CJUMP.false, NULL))));
       break;
     }
@@ -139,7 +139,7 @@ static Temp_temp munchExp(T_exp e) {
           case T_div: result = a / b; break;
           default: assert(0);
         }
-        S("li `d0, %d", result);
+        S("li `d0, %d\n", result);
         emit(AS_Oper(strdup(buf), L(r, NULL), NULL, NULL));
         return r;
       } else if ((e1->kind == T_CONST || e2->kind == T_CONST) &&
@@ -151,8 +151,8 @@ static Temp_temp munchExp(T_exp e) {
         T_exp var = e1->kind == T_CONST ? e2 : e1;
         int constt = e1->kind == T_CONST ? e1->u.CONST : e2->u.CONST;
         switch (op) {
-          case T_plus: S("addi `d0, `s0, %d", constt); break;
-          case T_minus: S("addi `d0, `s0, -%d", constt); break;
+          case T_plus: S("addi `d0, `s0, %d\n", constt); break;
+          case T_minus: S("addi `d0, `s0, -%d\n", constt); break;
           default: assert(0);
         }
         emit(AS_Oper(strdup(buf), L(r, NULL), L(munchExp(var), NULL), NULL));
@@ -160,10 +160,10 @@ static Temp_temp munchExp(T_exp e) {
       } else {
         /* BINOP(*,e1,e2) */
         switch (op) {
-          case T_plus: S("add `d0, `s0, `s1"); break;
-          case T_minus: S("sub `d0, `s0, `s1"); break;
-          case T_mul: S("mul `d0, `s0, `s1"); break;
-          case T_div: S("div `d0, `s0, `s1"); break;
+          case T_plus: S("add `d0, `s0, `s1\n"); break;
+          case T_minus: S("sub `d0, `s0, `s1\n"); break;
+          case T_mul: S("mul `d0, `s0, `s1\n"); break;
+          case T_div: S("div `d0, `s0, `s1\n"); break;
           default: assert(0);
         }
         emit(AS_Oper(strdup(buf), L(r, NULL),
@@ -178,17 +178,17 @@ static Temp_temp munchExp(T_exp e) {
         T_binOp op = e->u.MEM->u.BINOP.op;
         if (op == T_plus && e2->kind == T_CONST) {
           /* MEM(BINOP(PLUS,e1,CONST(i))) */
-          S("lw `d0, %d(`s0)", e2->u.CONST);
+          S("lw `d0, %d(`s0)\n", e2->u.CONST);
           emit(AS_Oper(strdup(buf), L(r, NULL), L(munchExp(e1), NULL), NULL));
           return r;
         } else if (op == T_plus && e1->kind == T_CONST) {
           /* MEM(BINOP(PLUS,CONST(i),e2)) */
-          S("lw `d0, %d(`s0)", e1->u.CONST);
+          S("lw `d0, %d(`s0)\n", e1->u.CONST);
           emit(AS_Oper(strdup(buf), L(r, NULL), L(munchExp(e2), NULL), NULL));
           return r;
         } else {
           /* MEM(e) */
-          emit(AS_Oper("lw `d0, 0(`s0)", L(r, NULL),
+          emit(AS_Oper("lw `d0, 0(`s0)\n", L(r, NULL),
                        L(munchExp(e->u.MEM), NULL), NULL));
           return r;
         }
@@ -199,7 +199,7 @@ static Temp_temp munchExp(T_exp e) {
         return r;
       } else {
         /* MEM(e) */
-        emit(AS_Oper("lw `d0, 0(`s0)", L(r, NULL), L(munchExp(e->u.MEM), NULL),
+        emit(AS_Oper("lw `d0, 0(`s0)\n", L(r, NULL), L(munchExp(e->u.MEM), NULL),
                      NULL));
         return r;
       }
@@ -210,13 +210,13 @@ static Temp_temp munchExp(T_exp e) {
     }
     case T_CONST: {
       /* CONST(i) */
-      S("li `d0, %d", e->u.CONST);
+      S("li `d0, %d\n", e->u.CONST);
       emit(AS_Oper(strdup(buf), L(r, NULL), NULL, NULL));
       return r;
     }
     case T_NAME: {
       /* NAME(lab) */
-      S("la `d0, %s", Temp_labelstring(e->u.NAME));
+      S("la `d0, %s\n", Temp_labelstring(e->u.NAME));
       emit(AS_Oper(strdup(buf), L(r, NULL), NULL, NULL));
       return r;
     }
