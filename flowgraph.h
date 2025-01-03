8afc71610fe66a2e8399615aabcc0831f0d72529
WilliamQiufeng <williamqiufeng@outlook.com>
2025-01-03 12:19:04 +0000

    add template

---
+++ flowgraph.h
@@ -0,0 +1,17 @@
+/*
+ * flowgraph.h - Function prototypes to represent control flow graphs.
+ */
+
+#ifndef FLOWGRAPH_H
+#define FLOWGRAPH_H
+
+#include "assem.h"
+#include "graph.h"
+#include "temp.h"
+
+Temp_tempList FG_def(G_node n);
+Temp_tempList FG_use(G_node n);
+bool FG_isMove(G_node n);
+G_graph FG_AssemFlowGraph(AS_instrList il);
+
+#endif
